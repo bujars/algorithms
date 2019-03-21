cdb2d95ef59b29f2622b2b7111dd4eba214f7291
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2019-03-20 22:41:46 -0400

    added note to bSA

--- binarySearchAlgorithm.cpp
+++ binarySearchAlgorithm.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 using namespace std;
 
+/*NOTE this is the code for linear search. Just wrote it here to test out a file. Will be updating soon.*/
 bool binarySearchAlgorithm(int A[], size_t n, int x){
 	for(size_t i = 0; i < n; i++){
 		if(A[i]==x){
